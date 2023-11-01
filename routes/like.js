1fd9c3929adca5b465f773ad1afd66ad030143ab
Hyojin Lee <rabbit3456@naver.com>
2023-11-01 19:31:33 +0900

    like 수정

--- routes/like.js
+++ routes/like.js
@@ -4,32 +4,45 @@ const db = require("../config/db");
 const passport = require("../config/passport");
 const { isLoggedIn} = require("../config/middleware");
 
-// 좋아요 기능
-router.post("/:postId", isLoggedIn, async (req, res) => {
+//좋아요 중복기능
+router.get("/:postId", isLoggedIn, async (req, res) => {
   try {
-    const { userId, postId } = req.body;
-    
-    // 좋아요 중복체크
+    const { userId } = req.user;
+    const { postId } = req.params;
+
     const checkSql = "SELECT * FROM likes WHERE userId = ? AND postId = ?";
     const [rows] = await db.query(checkSql, [userId, postId]);
 
     if (rows.length > 0) {
-      res.status(400).json({ message: "이미 좋아요를 눌렀습니다." });
+      res.status(200).json({ message: "이미 좋아요를 눌렀습니다.", isLiked: true });
     } else {
-      // 좋아요 기능
-      const likedSql = "INSERT INTO likes (userId, postId) VALUES (?, ?)";
-      await db.query(likedSql, [userId, postId]);
-
-      const updatePostSql = "UPDATE postings SET likesCount = likesCount + 1 WHERE id = ?";
-      await db.query(updatePostSql, [postId]);
-
-      res.status(200).json({ message: "좋아요!" });
+      res.status(200).json({ message: "좋아요를 누를 수 있습니다.", isLiked: false });
     }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "서버 오류입니다." });
   }
 });
+
+//좋아요 기능
+router.post("/:postId", isLoggedIn, async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const { postId } = req.params;
+
+    const likedSql = "INSERT INTO likes (userId, postId) VALUES (?, ?)";
+    await db.query(likedSql, [userId, postId]);
+
+    const updatePostSql = "UPDATE postings SET likesCount = likesCount + 1 WHERE id = ?";
+    await db.query(updatePostSql, [postId]);
+
+    res.status(200).json({ message: "좋아요!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 오류입니다." });
+  }
+});
+
 
   
   // 좋아요 취소 기능
