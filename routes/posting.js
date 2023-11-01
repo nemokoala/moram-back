a01a821431fcd76da393732a960c6d549d562d89
leeyeonju02 <dlduswn0783@naver.com>
2023-11-01 18:03:21 +0900

    어드민 페이지 병합전 커밋

--- routes/posting.js
+++ routes/posting.js
@@ -2,23 +2,24 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 const passport = require("../config/passport");
-const { isLoggedIn} = require("../config/middleware");
+const { isLoggedIn } = require("../config/middleware");
 
 router.get("/test", (req, res) => {
   res.send("test");
   console.log(req.session.passport);
 });
 
- //과 게시글 조회
+//과 게시글 조회
 router.get("/", async (req, res) => {
   try {
     let { category, tag } = req.query;
-    let titleSql = "SELECT id, title, nickname, writeTime, hitCount, likesCount, tag, category FROM postings";
+    let titleSql =
+      "SELECT id, title, nickname, writeTime, hitCount, likesCount, tag, category FROM postings";
     let queryParams = [];
 
     if (category || tag) {
       let conditions = [];
-      
+
       if (category) {
         conditions.push("category = ?");
         queryParams.push(category);
@@ -31,10 +32,9 @@ router.get("/", async (req, res) => {
 
       titleSql += " WHERE " + conditions.join(" AND ");
     }
-    
+
     const [results] = await db.query(titleSql, queryParams);
     return res.json(results);
-    
   } catch (error) {
     res.status(500).json({ message: "서버 오류입니다." });
     console.error(error);
@@ -44,64 +44,99 @@ router.get("/", async (req, res) => {
 // 특정 게시글 조회
 router.get("/:id", async (req, res) => {
   try {
-    const postingSql = "SELECT * FROM postings WHERE id = ?"
-    const [results] = await db.query( postingSql, [req.params.id] );
+    const postingSql = "SELECT * FROM postings WHERE id = ?";
+    const [results] = await db.query(postingSql, [req.params.id]);
     if (results.length === 0) {
       return res.status(404).json({ message: "게시물을 찾을 수 없습니다." });
     }
-  // 조회수 증가
-    const updateHitCountSql = "UPDATE postings SET hitCount = hitCount + 1 WHERE id = ?";
+    // 조회수 증가
+    const updateHitCountSql =
+      "UPDATE postings SET hitCount = hitCount + 1 WHERE id = ?";
     await db.query(updateHitCountSql, [req.params.id]);
-    
+
     res.json(results[0]);
-    
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "서버 오류입니다." });
   }
 });
 
-// 새로운 게시글 추가 
+// 새로운 게시글 추가
 router.post("/add", isLoggedIn, async (req, res) => {
   const { title, content, img1Url, img2Url, img3Url, category, tag } = req.body;
   // 여기서 세션으로부터 userId와 nickname을 가져옵니다.
   const { userId, nickname } = req.session.passport.user;
-  
+
   try {
-    const addSql = "INSERT INTO postings (userId, nickname, writeTime, updateTime, title, content, img1Url, img2Url, img3Url, category, tag)\
+    const addSql =
+      "INSERT INTO postings (userId, nickname, writeTime, updateTime, title, content, img1Url, img2Url, img3Url, category, tag)\
      VALUES (?, ?, NOW(), NOW(), ?, ?, ?, ?, ?, ?, ?)";
 
     // 물음표에 해당하는 값들을 배열로 전달
-    const values = [userId, nickname, title, content, img1Url, img2Url, img3Url, category, tag];
+    const values = [
+      userId,
+      nickname,
+      title,
+      content,
+      img1Url,
+      img2Url,
+      img3Url,
+      category,
+      tag,
+    ];
     const [results] = await db.query(addSql, values);
     res.status(201).json({ message: "게시물 작성이 완료되었습니다." });
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "서버 오류입니다." });
   }
 });
 
-
-
 // 특정 게시글 업데이트
-router.put("/update/:id", isLoggedIn , async (req, res) => {
-  const { userId, nickname, writeTime, updateTime,
-          title,content,img1Url,img2Url,img3Url,
-          likesCount,hitCount ,category ,tag } = req.body;
-  
+router.put("/update/:id", isLoggedIn, async (req, res) => {
+  const {
+    userId,
+    nickname,
+    writeTime,
+    updateTime,
+    title,
+    content,
+    img1Url,
+    img2Url,
+    img3Url,
+    likesCount,
+    hitCount,
+    category,
+    tag,
+  } = req.body;
+
   try {
-    const updateSql = "UPDATE postings SET userId=?, nickname=?, writeTime=?, updateTime=?, title=?, content=?, img1Url=?, img2Url=?, img3Url=?,\
-    likesCount=? ,hitCount=? ,category=? ,tag=? WHERE id = ?"
-    const [results] = await db.query( updateSql, [userId,nickname ,writeTime ,updateTime,title,content,img1Url,img2Url,img3Url,
-        likesCount,hitCount ,category ,tag, req.params.id]);
-    
-    if (results.affectedRows === 0) { //length성능차이 있는지
+    const updateSql =
+      "UPDATE postings SET userId=?, nickname=?, writeTime=?, updateTime=?, title=?, content=?, img1Url=?, img2Url=?, img3Url=?,\
+    likesCount=? ,hitCount=? ,category=? ,tag=? WHERE id = ?";
+    const [results] = await db.query(updateSql, [
+      userId,
+      nickname,
+      writeTime,
+      updateTime,
+      title,
+      content,
+      img1Url,
+      img2Url,
+      img3Url,
+      likesCount,
+      hitCount,
+      category,
+      tag,
+      req.params.id,
+    ]);
+
+    if (results.affectedRows === 0) {
+      //length성능차이 있는지
       return res.status(404).json({ message: "게시물을 찾을 수 없습니다." });
     }
-    
+
     res.json({ message: "게시물이 수정되었습니다." });
-    
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "서버 오류입니다." });
@@ -109,21 +144,20 @@ router.put("/update/:id", isLoggedIn , async (req, res) => {
 });
 
 // 특정 게시글 삭제 //user 인증 기능 하면 추가.
-router.delete("/delete/:id", isLoggedIn , async(req,res)=>{
-   try {
-     const deleteSql = "DELETE FROM postings WHERE id = ?"
-     const [results] = await db.query( deleteSql, [req.params.id]);
-     
-     if (results.affectedRows === 0) {
-       return res.status(404).json({ message: "게시물을 찾을 수 없습니다." });
-     }
+router.delete("/delete/:id", isLoggedIn, async (req, res) => {
+  try {
+    const deleteSql = "DELETE FROM postings WHERE id = ?";
+    const [results] = await db.query(deleteSql, [req.params.id]);
 
-     res.json({ message: "게시물이 삭제되었습니다." });
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ message: "게시물을 찾을 수 없습니다." });
+    }
 
-   } catch(error){
-     console.error(error);
-     res.status(500).json({message:"서버 오류입니다."});
-   }
+    res.json({ message: "게시물이 삭제되었습니다." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 오류입니다." });
+  }
 });
 
 module.exports = router;
