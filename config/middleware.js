5e5e9c1cafea63e700e68dac85c28ff7a6ed6694
nemokoala <fdxguy2@gmail.com>
2023-11-06 23:21:11 +0900

    isLoggedIn return 값 수정

--- config/middleware.js
+++ config/middleware.js
@@ -4,7 +4,7 @@ exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next(); // 다음 미들웨어
   } else {
-    res.status(403).send("로그인 필요");
+    res.status(403).json({ message: "로그인 필요합니다." });
   }
 };
 
