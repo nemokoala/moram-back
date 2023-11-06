3d830071b5f31a5a4e8394c6c73e75c023c22ccc
leeyeonju02 <dlduswn0783@naver.com>
2023-11-06 21:17:16 +0900

    어드민 페이지 공지글, 게시글 기능

---
+++ routes/notice.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/db");
+const passport = require("../config/passport");
+const { isnotloggedin, isLoggedIn, isAdmin } = require("../config/middleware");
+
+//공지기능
+//1.1 유저 : 전체 공지 조회 hide = null인 값만 출력
+router.get("/user", isLoggedIn, async (req, res) => {
+  try {
+    const allSql =
+      "SELECT id, title, writeTime, updateTime FROM notices WHERE hide IS NULL";
+    const [results] = await db.query(allSql);
+    return res.json(results);
+  } catch (error) {
+    res.status(500).json({ message: "db에러" });
+    console.error(error);
+  }
+});
+
+//1.2 유저 : 특정 공지 조회 - hide = null이면서 id 파라미터가 일치하는 데이터
+router.get("/user/:id", isLoggedIn, async (req, res) => {
+  const noticesId = Number(req.params.id);
+  try {
+    const getSql = "SELECT * FROM notices WHERE hide IS NULL AND id = ?";
+    const [results] = await db.query(getSql, [noticesId]);
+    if (results.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "해당 공지를 찾을 수 없습니다. " });
+    }
+    res.json(results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 에러" });
+  }
+});
+
+//2.1 관리자: 전체 공지 조회
+router.get("/admin", isLoggedIn, isAdmin, async (req, res) => {
+  try {
+    const allSql = "SELECT id, title, writeTime, nickname, hide FROM notices";
+    const [results] = await db.query(allSql);
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ message: "db에러" });
+    console.error(error);
+  }
+});
+
+//2.2 관리자: 특정 공지 조회
+router.get("/admin/:id", isLoggedIn, isAdmin, async (req, res) => {
+  const noticesId = req.params.id;
+  try {
+    const getSql = "SELECT * FROM notices  WHERE id = ?";
+    const [results] = await db.query(getSql, [noticesId]);
+    if (results.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "해당 공지를 찾을 수 없습니다. " });
+    }
+    res.json(results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 에러" });
+  }
+});
+
+//3. 관리자가 공지 추가하기
+router.post("/", isLoggedIn, isAdmin, async (req, res) => {
+  try {
+    const writeTime = new Date();
+    const { title, content } = req.body;
+    const adminId = Number(req.session.passport.user[0].id);
+    const adminNickname = req.session.passport.user[0].nickname;
+
+    const addSql =
+      "INSERT INTO notices (userId, title, content, writeTime, nickname) VALUES (?, ?, ?, ?, ?)";
+
+    const values = [adminId, title, content, writeTime, adminNickname];
+
+    const [results] = await db.query(addSql, values);
+    res.status(201).json({
+      message: "공지 작성 완료.",
+      comment: {
+        title,
+        content,
+        writeTime,
+        adminNickname,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "공지 추가 서버에러 " });
+  }
+});
+
+//4. 관리자 공지 삭제 기능
+router.delete("/:id", isLoggedIn, isAdmin, async (req, res) => {
+  const noticesId = Number(req.params.id);
+  try {
+    const deleteSql = "DELETE FROM notices WHERE id=? ";
+    const [results] = await db.query(deleteSql, [noticesId]);
+
+    if (results.length === 0) {
+      res.status(404).send("해당하는 공지글이 더 이상 존재하지 않습니다.");
+      return;
+    }
+    res.status(200).send("공지글이 삭제되었습니다.");
+  } catch (error) {
+    res.status(500).json({ message: "서버 에러" });
+    console.error(error);
+  }
+});
+
+module.exports = router;
