639c8cb3c88c1f3ecaa56c63b771a7b51c901025
nemokoala <fdxguy2@gmail.com>
2023-11-07 21:52:59 +0900

    secure true

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      //secure: true, // HTTPS를 통해서만 쿠키가 전송되도록 설정
+      secure: true, // HTTPS를 통해서만 쿠키가 전송되도록 설정
       httpOnly: true, // 클라이언트 JavaScript가 쿠키를 접근하지 못하도록 설정
       sameSite: "None",
     },
