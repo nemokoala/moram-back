33070c36a487df5533547746862d9c41727103d8
nemokoala <fdxguy2@gmail.com>
2023-11-08 19:20:44 +0900

    이미지 업로드 수정

--- config/aws.js
+++ config/aws.js
@@ -19,25 +19,22 @@ async function generatePresignedUrl(params) {
 
 const getUploadUrls = async (req, res, next) => {
   const user = req.session.passport.user[0];
-  const { imageCount } = req.body;
 
   try {
     const bucket = "moram";
-    let uploadData = [];
+    let uploadData;
 
-    for (let i = 0; i < imageCount; i++) {
-      const key = `community/${user.nickname}_${new Date().getTime()}_${i}`;
-      const params = {
-        Bucket: bucket,
-        Key: key,
-        Expires: 60 * 5,
-        ContentType: "image/jpeg",
-      };
+    const key = `community/${user.nickname}_${new Date().getTime()}`;
+    const params = {
+      Bucket: bucket,
+      Key: key,
+      Expires: 60 * 5,
+      ContentType: "image/jpeg",
+    };
 
-      const presignedUrl = await generatePresignedUrl(params);
-      const imageUrl = `https://${bucket}.s3.${s3.config.region}.amazonaws.com/${key}`;
-      uploadData.push({ presignedUrl: presignedUrl, imageUrl: imageUrl });
-    }
+    const presignedUrl = await generatePresignedUrl(params);
+    const imageUrl = `https://${bucket}.s3.${s3.config.region}.amazonaws.com/${key}`;
+    uploadData = { presignedUrl: presignedUrl, imageUrl: imageUrl };
 
     req.uploadData = uploadData; // 미들웨어에서 다음 처리를 위해 uploadData를 요청 객체에 첨부
     next();
