49bf9cb73a9a7c038c332c9732167fe6fb0abe97
nemokoala <fdxguy2@gmail.com>
2024-01-20 01:53:01 +0900

    db config aws에 맞게 수정

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 // 데이터베이스 연결 정보 설정
 const db = mysql.createPool({
-  host: "svc.sel5.cloudtype.app", // MySQL 서버 호스트 이름 또는 IP 주소
-  port: 31760, // MySQL 서버 포트 번호
-  user: "root", // MySQL 사용자 이름
-  password: "wkumoram", // MySQL 사용자 비밀번호
-  database: "moram", // 사용할 데이터베이스 이름
+  host: process.env.DB_HOST, // MySQL 서버 호스트 이름 또는 IP 주소
+  port: process.env.DB_PORT, // MySQL 서버 포트 번호
+  user: process.env.DB_USER, // MySQL 사용자 이름
+  password: process.env.DB_PASSWORD, // MySQL 사용자 비밀번호
+  database: "moramDB", // 사용할 데이터베이스 이름
 });
 
 module.exports = db;
